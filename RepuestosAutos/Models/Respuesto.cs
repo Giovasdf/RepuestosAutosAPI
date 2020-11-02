3ebdaf7161ee692f744b9e1a96633ee0a3310434
Giovanni Molina <gi.molina@profesor.duoc.cl>
2020-11-02 11:21:15 -0300

    Carpetas Agregadas Azure y Models

---
+++ RepuestosAutos/Models/Respuesto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RepuestosAutos.Models
+{
+    public class Respuesto
+    {
+        public int idRespuesto { get; set; }
+        public string nombreRepuesto { get; set; }
+
+    }
+}
